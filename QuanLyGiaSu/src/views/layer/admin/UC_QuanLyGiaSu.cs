4e667f950fa839dc855522e506f3358612ad558c
TrunNghia232 <trungnghiaazd@gmail,com>
2022-10-23 23:40:46 +0700

    fix

--- QuanLyGiaSu/src/views/layer/admin/UC_QuanLyGiaSu.cs
+++ QuanLyGiaSu/src/views/layer/admin/UC_QuanLyGiaSu.cs
@@ -46,18 +46,105 @@ namespace QuanLyGiaSu.src.app.views.layer
         private void SuaThongTinGiaSu_Click(object sender, EventArgs e)
         {
             SuaGiaSu suaGiaSu1 = new SuaGiaSu();
-            suaGiaSu1.ShowThongTinGiaSu(dgvQuanLyGiaSu.CurrentRow.Cells[0].Value.ToString(),
-                dgvQuanLyGiaSu.CurrentRow.Cells[11].Value.ToString(),
-                dgvQuanLyGiaSu.CurrentRow.Cells[2].Value.ToString(),
-                dgvQuanLyGiaSu.CurrentRow.Cells[5].Value.ToString(),
-                dgvQuanLyGiaSu.CurrentRow.Cells[3].Value.ToString(),
-                dgvQuanLyGiaSu.CurrentRow.Cells[6].Value.ToString(),
-                dgvQuanLyGiaSu.CurrentRow.Cells[4].Value.ToString(),
-                dgvQuanLyGiaSu.CurrentRow.Cells[13].Value.ToString(),
-                dgvQuanLyGiaSu.CurrentRow.Cells[9].Value.ToString(),
-                dgvQuanLyGiaSu.CurrentRow.Cells[12].Value.ToString(),
-                dgvQuanLyGiaSu.CurrentRow.Cells[10].Value.ToString()
-                );
+            try                  
+            {
+                string truongDT;
+                string hoTen;
+                string ngaySinh;
+                string gioiTinh;
+                string soDienThoai;
+                string cmnd;
+                string diaChi;
+                string queQuan;
+                string uuDiem;
+                string trinhDo;
+                if (dgvQuanLyGiaSu.CurrentRow.Cells[11].Value.ToString() == null)
+                {
+                    truongDT = "";
+                }
+                else
+                    truongDT = dgvQuanLyGiaSu.CurrentRow.Cells[11].Value.ToString();
+
+                if (dgvQuanLyGiaSu.CurrentRow.Cells[2].Value.ToString() == null)
+                {
+                    hoTen = "";
+                }
+                else
+                    hoTen = dgvQuanLyGiaSu.CurrentRow.Cells[2].Value.ToString();
+                if (dgvQuanLyGiaSu.CurrentRow.Cells[5].Value.ToString() == null)
+                {
+                    ngaySinh = "";
+                }
+                else
+                    ngaySinh = dgvQuanLyGiaSu.CurrentRow.Cells[5].Value.ToString();
+
+                if (dgvQuanLyGiaSu.CurrentRow.Cells[3].Value.ToString() == null)
+                {
+                    gioiTinh = "";
+                }
+                else
+                    gioiTinh = dgvQuanLyGiaSu.CurrentRow.Cells[3].Value.ToString();
+
+                if (dgvQuanLyGiaSu.CurrentRow.Cells[6].Value.ToString() == null)
+                {
+                    soDienThoai = "";
+                }
+                else
+                    soDienThoai = dgvQuanLyGiaSu.CurrentRow.Cells[6].Value.ToString();
+
+                if (dgvQuanLyGiaSu.CurrentRow.Cells[4].Value.ToString() == null)
+                {
+                    cmnd = "";
+                }
+                else
+                    cmnd = dgvQuanLyGiaSu.CurrentRow.Cells[4].Value.ToString();
+
+                if (dgvQuanLyGiaSu.CurrentRow.Cells[13].Value == null)
+                {
+                    diaChi = "";
+                }
+                else
+                    diaChi = dgvQuanLyGiaSu.CurrentRow.Cells[13].Value.ToString();
+
+                if (dgvQuanLyGiaSu.CurrentRow.Cells[9].Value.ToString() == null)
+                {
+                    queQuan = "";
+                }
+                else
+                    queQuan = dgvQuanLyGiaSu.CurrentRow.Cells[9].Value.ToString();
+
+                if (dgvQuanLyGiaSu.CurrentRow.Cells[12].Value.ToString() == null)
+                {
+                    uuDiem = "";
+                }
+                else
+                    uuDiem = dgvQuanLyGiaSu.CurrentRow.Cells[12].Value.ToString();
+
+                if (dgvQuanLyGiaSu.CurrentRow.Cells[10].Value.ToString() == null)
+                {
+                    trinhDo = "";
+                }
+                else
+                    trinhDo = dgvQuanLyGiaSu.CurrentRow.Cells[10].Value.ToString();
+
+
+                suaGiaSu1.ShowThongTinGiaSu(dgvQuanLyGiaSu.CurrentRow.Cells[0].Value.ToString(),
+                               truongDT,
+                               hoTen,
+                               ngaySinh,
+                               gioiTinh,
+                               soDienThoai,
+                               cmnd,
+                               diaChi,
+                               queQuan,
+                               uuDiem,
+                               trinhDo
+                               );
+            } catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
             suaGiaSu1.Show();
         }
 
